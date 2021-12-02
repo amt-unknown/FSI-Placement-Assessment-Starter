7ff08be13fbe0caeea86cbdbed8008aeb149e450
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2021-12-01 18:53:59 -0800

    Update to include semicolon

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,61 @@
 // First, tell us your name
-let yourName = "Andrew M Thompson" // HINT: Replace this with your own name!
+let yourName = "Andrew M Thompson";
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
-let total = 0   //Total Order Size
+let gb = 0;      // Gingerbread
+let cc = 0;      // Chocolate Chip
+let sugar = 0;   // Sugar Sprinkle
+let total = 0;   //Total Order Size
 
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+document.getElementById('credit').textContent = `Created by ${yourName}`;
 
 
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
-    gb += 1
-    document.getElementById('qty-gb').textContent = gb
-    document.getElementById('qty-total').textContent = gb + cc + sugar
-})
+    gb += 1;
+    document.getElementById('qty-gb').textContent = gb;
+    document.getElementById('qty-total').textContent = gb + cc + sugar;
+});
 // Event listener for clicks on the "-" button for Gingerbread cookies
 document.getElementById('minus-gb').addEventListener('click', function() {
     if(gb > 0){
-        gb -= 1
-        document.getElementById('qty-gb').textContent = gb
-        document.getElementById('qty-total').textContent = gb + cc + sugar
-    } 
-})
+        gb -= 1;
+        document.getElementById('qty-gb').textContent = gb;
+        document.getElementById('qty-total').textContent = gb + cc + sugar;
+    }; 
+});
 
 
 
 // Event listener for clicks on the "+" button for Chocolate Chip cookies
 document.getElementById('add-cc').addEventListener('click', function() {
-    cc += 1
-    document.getElementById('qty-cc').textContent = cc
-    document.getElementById('qty-total').textContent = gb + cc + sugar
-})
+    cc += 1;
+    document.getElementById('qty-cc').textContent = cc;
+    document.getElementById('qty-total').textContent = gb + cc + sugar;
+});
 // Event listener for clicks on the "-" button for Chocolate Chip cookies
 document.getElementById('minus-cc').addEventListener('click', function() {
     if(cc > 0){
-        cc -= 1
-        document.getElementById('qty-cc').textContent = cc
-        document.getElementById('qty-total').textContent = gb + cc + sugar
-    } 
-})
+        cc -= 1;
+        document.getElementById('qty-cc').textContent = cc;
+        document.getElementById('qty-total').textContent = gb + cc + sugar;
+    };
+});
 
 
 // Event listener for clicks on the "+" button for Sugar cookies
 document.getElementById('add-sugar').addEventListener('click', function() {
-    sugar += 1
-    document.getElementById('qty-sugar').textContent = sugar
-    document.getElementById('qty-total').textContent = gb + cc + sugar
-})
+    sugar += 1;
+    document.getElementById('qty-sugar').textContent = sugar;
+    document.getElementById('qty-total').textContent = gb + cc + sugar;
+});
 // Event listener for clicks on the "-" button for Sugar cookies
 document.getElementById('minus-sugar').addEventListener('click', function() {
     if(sugar > 0){
-        sugar -= 1
-        document.getElementById('qty-sugar').textContent = sugar
-        document.getElementById('qty-total').textContent = gb + cc + sugar
-    } 
-})
+        sugar -= 1;
+        document.getElementById('qty-sugar').textContent = sugar;
+        document.getElementById('qty-total').textContent = gb + cc + sugar;
+    }; 
+});
